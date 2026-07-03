0adf4bc2d54354d317a059c52b5e5c2229eee207
Mercyn2 <bugoyinyoyow@gmail.com>
2026-07-03 22:22:21 +0800

    Update logger.lua added ( prints color )

--- o4/logger.lua
+++ o4/logger.lua
@@ -1,6 +1,37 @@
-local l={}
-function l:info(m)print("[INFO] "..m)end
-function l:warn(m)print("[WARN] "..m)end
-function l:error(m)error("[ERROR] "..m,2)end
-return l
+-- add prints color :)
+local colors = {
+  reset = "\27[0m",
+  red = "\27[31m",
+  green = "\27[32m",
+  yellow = "\27[33m",
+  blue = "\27[34m",
+  magenta = "\27[35m",
+  cyan = "\27[36m",
+  white = "\27[37m",
+  brightRed = "\27[91m",
+  brightGreen = "\27[92m",
+  brightYellow = "\27[93m",
+  brightBlue = "\27[94m",
+  brightMagenta = "\27[95m",
+  brightCyan = "\27[96m",
+  brightWhite = "\27[97m",
+}
 
+local function colorize(text, color)
+  return colors[color] .. text .. colors.reset
+end
+
+local l = {}
+function l:info(m)
+  print(colorize("[INFO] ", "green") .. m)
+end
+
+function l:warn(m)
+  print(colorize("[WARN] ", "yellow") .. m)
+end
+
+function l:error(m)
+  error(colorize("[ERROR] ", "red") .. m, 2)
+end
+
+return l
