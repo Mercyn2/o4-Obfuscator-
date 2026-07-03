516948ee674f05182820ad83c8c0f927ebd453e9
Mercyn2 <bugoyinyoyow@gmail.com>
2026-07-03 22:00:27 +0800

    Create number.lua

---
+++ o4/steps/vmify/expressions/number.lua
@@ -0,0 +1,14 @@
+local A = require "ast"
+return function(self, expr, funcDepth, n)
+  local scope = self.activeBlock.scope
+  local regs  = {}
+  for i = 1, n do
+    regs[i] = self:allocRegister()
+    if i == 1 then
+      self:addStatement(self:setRegister(scope, regs[i], A.NumberExpression(expr.value)), {regs[i]}, {}, false)
+    else
+      self:addStatement(self:setRegister(scope, regs[i], A.NilExpression()), {regs[i]}, {}, false)
+    end
+  end
+  return regs
+end
