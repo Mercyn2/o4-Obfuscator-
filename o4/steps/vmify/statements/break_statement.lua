7f1137482b7f5b6c8b1a99f5fddfe815c8caf96c
Mercyn2 <bugoyinyoyow@gmail.com>
2026-07-05 00:42:55 +0800

    Update break_statement.lua fixes

--- o4/steps/vmify/statements/break_statement.lua
+++ o4/steps/vmify/statements/break_statement.lua
@@ -12,12 +12,13 @@ return function(self, stmt, funcDepth)
 
   for _, v in ipairs(toFree) do
     local varReg = self:getVarRegister(v.scope, v.id, nil, nil)
-    if self:isUpvalue(v.scope, v.id) then
-      scope:addReferenceToHigherScope(self.scope, self.freeUpvalVar)
-      self:addStatement(self:setRegister(scope, varReg, A.FunctionCallExpression(A.VariableExpression(self.scope, self.freeUpvalVar), { self:register(scope, varReg) })), {varReg}, {varReg}, false)
-    else
-      self:addStatement(self:setRegister(scope, varReg, A.NilExpression()), {varReg}, {}, false)
-    end
+    -- Note: we deliberately don't runtime-free upvalue boxes here (via
+    -- freeUpvalVar) even for variables marked isUpvalue. A closure created
+    -- earlier in this iteration may have captured this box and can still
+    -- be called after the loop exits (e.g. stored in a table) -- freeing
+    -- the box here would wipe that value out from under it. Just clear
+    -- the local register that held the box id/value.
+    self:addStatement(self:setRegister(scope, varReg, A.NilExpression()), {varReg}, {}, false)
   end
   self:addStatement(self:setPos(scope, stmt.loop.__final_block.id), {self.POS_REGISTER}, {}, false)
   self.activeBlock.advanceToNextBlock = false
